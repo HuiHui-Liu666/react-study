e13069ce2fd24bb89a8cd0f6ebc98f21026c923a
Hanyu <191077050@qq.com>
2024-01-25 14:47:45 +0800

    feat:在react里面配置redux
    6.1 创建子模块js
    6.2 创建合并组模块的index.js
    6.3 在入口函数注入store
    6.4 在组件里面引入使用。

--- react-bill-test/src/pages/Layout/index.js
+++ react-bill-test/src/pages/Layout/index.js
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router-dom";
 import { Button } from "antd-mobile";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import {getBillList} from '@/store/modules/billStore'
 const Layout = ()=>{
+    const dispatch = useDispatch();
+    // 组件上来的时候出发一次
+    useEffect(()=>{
+        dispatch(getBillList())
+    },[dispatch])
     return (
         <div>
             <Outlet></Outlet>
             layout
-            
+
         <Button
             color="primary"
             >csss</Button>
